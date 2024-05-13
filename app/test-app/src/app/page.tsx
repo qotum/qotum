8e80c6a6f4eb70559ee6915726cc8bf60f64b93a
ImJustLucas <lucas.bellier@icloud.com>
2024-05-13 23:55:27 +0200

    💄 update style brand

--- app/test-app/src/app/page.tsx
+++ app/test-app/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main
       className={css({
-        backgroundColor: "brands.accent",
+        backgroundColor: "brand",
       })}
     >
       test APP
