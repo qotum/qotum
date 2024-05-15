bad10492ca31eb95ca7f46fab5d3dd743b033bf4
ImJustLucas <lucas.bellier@icloud.com>
2024-05-15 09:51:34 +0200

    🔧 add spacings tokens

---
+++ packages/panda-preset/src/tokens/dimensions/spacing.ts
@@ -0,0 +1,40 @@
+export const spacing = {
+  padding: {
+    XL: { value: "32px" },
+    LG: { value: "24px" },
+    MD: {
+      DEFAULT: {
+        value: "16px",
+      },
+      nudge: {
+        value: "20px",
+      },
+    },
+    SM: {
+      DEFAULT: {
+        value: "8px",
+      },
+      nudge: {
+        value: "12px",
+      },
+    },
+    XS: {
+      DEFAULT: {
+        value: "4px",
+      },
+      nudge: {
+        value: "6px",
+      },
+    },
+    XXS: { value: "2px" },
+    0: { value: "0px" },
+  },
+  radius: {
+    rounded: { value: "9999px" },
+    XL: { value: "16px" },
+    LG: { value: "12px" },
+    MD: { value: "8px" },
+    SM: { value: "6px" },
+    XS: { value: "4px" },
+  },
+}
--- packages/panda-preset/src/tokens/index.ts
+++ packages/panda-preset/src/tokens/index.ts
@@ -1,8 +1,11 @@
-import type { Tokens } from "@pandacss/types";
-import { colors } from "./colors";
+import type { Tokens } from "@pandacss/types"
 
-const defineTokens = <T extends Tokens>(v: T) => v;
+import { colors } from "./colors"
+import { spacing } from "./dimensions/spacing"
+
+const defineTokens = <T extends Tokens>(v: T) => v
 
 export const tokens = defineTokens({
   colors,
-});
+  spacing,
+})
