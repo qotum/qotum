7f2aed1a4e0d3f4bbf741e649f1de0f95e6d2db4
ImJustLucas <lucas.bellier@icloud.com>
2024-05-15 09:53:54 +0200

    ♻️ update import statements in panda-preset

--- packages/panda-preset/src/tokens/index.ts
+++ packages/panda-preset/src/tokens/index.ts
@@ -1,11 +1,13 @@
 import type { Tokens } from "@pandacss/types"
 
 import { colors } from "./colors"
-import { spacing } from "./dimensions/spacing"
+import { spacing } from "./dimensions"
+import { fontWeights } from "./texts"
 
 const defineTokens = <T extends Tokens>(v: T) => v
 
 export const tokens = defineTokens({
   colors,
   spacing,
+  fontWeights,
 })
---
+++ packages/panda-preset/src/tokens/texts/index.ts
@@ -0,0 +1,6 @@
+export const fontWeights = {
+  bold: { value: "700" },
+  semibold: { value: "600" },
+  medium: { value: "500" },
+  regular: { value: "400" },
+}
