1b764191243d849bc72665d7e3fe8e9db5b00dae
ImJustLucas <lucas.bellier@icloud.com>
2024-05-16 21:26:33 +0200

    ✨ add design system component

---
+++ packages/design-system/panda.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "@pandacss/dev";
+import qotumPreset from "@qotum/panda-preset";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  presets: [qotumPreset],
+
+  // Where to look for your css declarations
+  include: [
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./src/app/**/*.{ts,tsx,js,jsx}",
+  ],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+
+  jsxFramework: "react",
+  // The output directory for your css system
+  outdir: "styled-system",
+});
---
+++ packages/design-system/src/components/button/index.ts
@@ -0,0 +1,4 @@
+import { button } from "../../../styled-system/recipes";
+import { styled } from "../../../styled-system/jsx";
+
+export const Button = styled("button", button);
