4c879e61ce0ebaa19c731750f6a8efc3557aafd3
jerembdn <work@jeremybdn.fr>
2024-08-12 13:58:46 +0200

    🚨 fix linter warnings

--- packages/panda-preset/src/components/image/index.tsx
+++ packages/panda-preset/src/components/image/index.tsx
@@ -1,20 +1,20 @@
-"use client";
+"use client"
 
-import { styled } from "@qotum/panda-exports/jsx";
-import { image } from "@qotum/panda-exports/recipes";
-import { default as NextImage } from "next/image";
-import type React from "react";
-import type { ComponentProps } from "react";
+import { styled } from "@qotum/panda-exports/jsx"
+import { image } from "@qotum/panda-exports/recipes"
+import { default as NextImage } from "next/image"
+import type React from "react"
+import type { ComponentProps } from "react"
 
-export type ImageProps = Exclude<ComponentProps<typeof NextImage>, "fill">;
+export type ImageProps = Exclude<ComponentProps<typeof NextImage>, "fill">
 
-const StyledImage = styled(NextImage, image);
+const StyledImage = styled(NextImage, image)
 
 const Image: React.FC<ImageProps> = ({
-	src,
-	alt = "",
-	...props
-	// @ts-ignore
-}: ImageProps) => <StyledImage src={src} alt={alt} {...props} />;
+  src,
+  alt = "",
+  ...props
+  // @ts-ignore
+}: ImageProps) => <StyledImage src={src} alt={alt} {...props} />
 
-export { Image };
+export { Image }
