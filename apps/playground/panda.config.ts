f2ff237304a2ad2b52d8ca2e3baecc2a3f0d34bf
ImJustLucas <lucas.bellier@icloud.com>
2024-09-28 12:50:37 +0200

    🩹 fix panda include

--- apps/playground/panda.config.ts
+++ apps/playground/panda.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "@pandacss/dev"
-import defaultConfig from "@qotum/panda-preset/panda.config"
+import { defineConfig } from "@pandacss/dev";
+import defaultConfig from "@qotum/panda-preset/panda.config";
 
 export default defineConfig({
   ...defaultConfig,
-  // Where to look for your css declarations
+
   include: [
     "./src/components/**/*.{ts,tsx,js,jsx}",
     "./src/app/**/*.{ts,tsx,js,jsx}",
   ],
   outdir: "../../packages/panda-exports/dist",
-})
+});
--- websites/landing/panda.config.ts
+++ websites/landing/panda.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from "@pandacss/dev";
 import defaultConfig from "@qotum/panda-preset/panda.config";
 
 export default defineConfig({
-	...defaultConfig,
-	// Where to look for your css declarations
-	include: [
-		"./src/components/**/*.{ts,tsx,js,jsx}",
-		"./src/app/**/*.{ts,tsx,js,jsx}",
-	],
-	outdir: "../../packages/panda-exports/dist",
+  ...defaultConfig,
+
+  include: ["./src/**/*.{ts,tsx,js,jsx}"],
+
+  outdir: "../../packages/panda-exports/dist",
 });
